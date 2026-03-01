6f53cdeddc42ec82f67473cae4df1576d8651c17
road2freedom77 <marcin.taranta@gmail.com>
2026-03-01 16:48:19 -0500

    google analytics integration!

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Lilita_One, Nunito } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import Script from "next/script";
 import "./globals.css";
 
 const lilitaOne = Lilita_One({
@@ -47,6 +48,20 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-X1WQ0GMZDN"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-X1WQ0GMZDN');
+            `}
+          </Script>
+        </head>
         <body className={`${lilitaOne.variable} ${nunito.variable}`}>
           {children}
         </body>
